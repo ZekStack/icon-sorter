77d4fd4ef9c5458793d172d24536a4571e7616a9
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-07-27 14:00:25 +0200

    Export confirm dialog API

---
+++ src/components/custom/confirm-dialog/index.ts
@@ -0,0 +1,9 @@
+export { ConfirmDialog } from "./confirm-dialog"
+export { ConfirmProvider } from "./confirm-provider"
+export { useConfirm } from "./use-confirm"
+export {
+  ConfirmError,
+  ConfirmSuccess,
+  ConfirmWarning,
+} from "./confirm-dialog-types"
+export type { ConfirmOptions, ConfirmType } from "./confirm-dialog-types"
