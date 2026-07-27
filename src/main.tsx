c2e1aa0c76f739684f3e43357a4ffe9e45aa0fc8
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-07-27 14:05:31 +0200

    Register shared dialog providers

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,21 @@ import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 import "./i18n"
+import { ConfirmProvider } from "@/components/custom/confirm-dialog"
+import { SelectDialogProvider } from "@/components/custom/select-dialog"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { IconSorterProvider } from "@/lib/icon-sorter-store"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <IconSorterProvider>
-        <App />
-      </IconSorterProvider>
+      <ConfirmProvider>
+        <SelectDialogProvider>
+          <IconSorterProvider>
+            <App />
+          </IconSorterProvider>
+        </SelectDialogProvider>
+      </ConfirmProvider>
     </ThemeProvider>
   </StrictMode>
 )
