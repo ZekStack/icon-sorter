8f694ad11015ad8a2adb72ec3c79e92ba2bbfd1f
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-07-27 14:02:33 +0200

    Export select dialog API

---
+++ src/components/custom/select-dialog/index.ts
@@ -0,0 +1,8 @@
+export { SelectDialog } from "./select-dialog"
+export { SelectDialogProvider } from "./select-dialog-provider"
+export { useSelect } from "./use-select"
+export type {
+  SelectDialogProps,
+  SelectDialogRequest,
+  SelectDialogValue,
+} from "./select-dialog-types"
